d67b931feea581a0a6c156c242f95f823a5729c0
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-09-13 12:06:43 +0200

    functions refactor

--- index.js
+++ index.js
@@ -2,26 +2,53 @@ fetch('https://gist.githubusercontent.com/Oskar-Dam/62e7175dc542af53a9d18cb29242
 .then(response => response.json())
 .then(data => {
     // Parte 1
+    console.log("-------------------------------------");
     donutWithMoreSugar(data);
+
+    console.log("-------------------------------------");
     donutWithMoreIron(data);
+
+    console.log("-------------------------------------");
     donutWithMoreProtein(data);
+
+    console.log("-------------------------------------");
     donutWithLessFiber(data);
 
+    console.log("-------------------------------------");
+
+
     // Parte 2 
+    console.log("-------------------------------------");
+    console.log("-------------------------------------");
+    console.log("-------------------------------------");
+
+
     listAllDonutsAndItsCarbs(data);
+    console.log("-------------------------------------");
+    listAllDonutsAndItsCals(data)
+    console.log("-------------------------------------");
+    showTheMeadiaOfCals(data);
+    console.log("-------------------------------------");
+    showTheTotalOfFatsSaturades(data);
+
 
 });
 
 function donutWithMoreSugar(data){
+
     let sweeter;
-    let gr = data.items.item[0].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars;
-    for(let i = 0; i < data.items.item.length; i++){
+    const donuts = listDonuts(data);
+    let gr = separateStringAndReturnInt(donuts[0].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars, "g");
 
-        const donut = data.items.item[i];
 
-        const sugar = donut.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars;
+    for(let i = 0; i < donuts.length; i++){
+
+        const donut = donuts[i];
+
+        const sugar = separateStringAndReturnInt(donut.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars, "g");
 
         if(gr < sugar){
+
             gr = sugar;
             sweeter = donut.name;
         }
@@ -33,9 +60,12 @@ function donutWithMoreSugar(data){
 function donutWithMoreIron(data){
     let name;
     let iron = 0;
-    for(let i = 0; i < data.items.item.length; i++){
 
-        const donut = data.items.item[i];
+    const donuts = listDonuts(data);
+
+    for(let i = 0; i < donuts.length; i++){
+
+        const donut = donuts[i];
 
         const vitamines  = donut.nutrition_facts.nutrition.vitamines;
 
@@ -45,13 +75,11 @@ function donutWithMoreIron(data){
             
             if(vitamine.type === 'Iron'){
 
-                const percent           = vitamine.percent.split('%');
-                let   percentNumber     = parseInt(percent[0]);
+                const percent           = separateStringAndReturnInt(vitamine.percent, '%');
                 
-
-                if(percentNumber > iron){
+                if(percent > iron){
                     
-                    iron = percentNumber;
+                    iron = percent;
                     name = donut.name;
                 }
             }
@@ -84,24 +112,56 @@ function donutWithMoreProtein(data){
 
 function donutWithLessFiber(data){
     let name;
-    let lessFibre = data.items.item[0].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre;
-
+    let fibreString = data.items.item[0].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre.split("g");
+    let lessFibre   = parseInt(fibreString[0]);
+    
     for(let i = 0; i < data.items.item.length; i++){
 
         const donut = data.items.item[i];
 
-        const fibre = donut.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre;
+        let aux     = donut.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre.split("g");
+        let fibre   = parseInt(aux[0]);
 
-        if(fibre < lessFibre){
+        if(fibre <= lessFibre){
 
             lessFibre = fibre;
             name = donut.name;
         }
     }
 
-    console.log("The donut with less fibre is " + name + " with " + lessFibre + "g");
+    console.log("The donut with less fibre is " + name + " with " + lessFibre + "g fibre");
 }
 
+function listAllDonutsAndItsCals(data){
+    const donuts = listDonuts(data);
+
+    for(let i = 0; i < donuts.length; i++){
+
+        const donut     = donuts[i];
+        const calories  = donut.nutrition_facts.nutrition.calories;
+
+        console.log(donut.name + " calories: " + calories);
+    }
+}
+
+function showTheMeadiaOfCals(data){
+
+    const donuts = listDonuts(data);
+    let   totalCalories = 0;
+
+    for(let i = 0; i < donuts.length; i++){
+
+        const donut = donuts[i];
+
+        const calories = donut.nutrition_facts.nutrition.calories;
+        totalCalories += calories;
+
+    }
+
+    const media = totalCalories / donuts.length;
+
+    console.log("The media of calories of the donuts is " + media);
+}
 
 function listAllDonutsAndItsCarbs(data){
 
@@ -111,8 +171,46 @@ function listAllDonutsAndItsCarbs(data){
 
         const carb  = donut.nutrition_facts.nutrition.carbohydrate.carbs_detail;
 
-        console.log(donut.name);
-        console.log(carb);
+        
+        console.log(donut.name + " carbohydrate amount " + carb.amount + " sugars: " + carb.type.sugars + "  fibre: " + carb.type.fibre );
+    }
+}
+
+function listDonuts(data){
+    const donuts = [];
+    for(let i = 0; i < data.items.item.length; i++){
+
+        const donut = data.items.item[i];
+
+        donuts.push(donut);
 
     }
+
+    return donuts;
+}
+
+function showTheTotalOfFatsSaturades(data){
+    
+    const donuts = listDonuts(data);
+    let totalCalories = 0;
+
+    for(let i = 0; i < donuts.length; i++){
+        
+        const donut = donuts[i];
+
+        const fatsSaturades = separateStringAndReturnInt(donut.nutrition_facts.nutrition.fat.fat_type.saturated, "g");
+
+        totalCalories += fatsSaturades;
+    }
+
+    console.log("La suma de la grasa saturada de todos los donuts es " + totalCalories);
+}
+
+function separateStringAndReturnInt(string, separator){
+
+    const aux       = string.split(separator);
+
+    const number    = parseInt(aux[0]);
+
+    return number;
 }
